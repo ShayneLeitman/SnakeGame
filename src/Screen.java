889b496369de56e9f29744fe4f4f0750a9f3b3c5
Shayne Leitman <sleitman17@gmail.com>
2020-01-08 21:24:43 -0500

    Randomizer works, static screen works

--- src/Screen.java
+++ src/Screen.java
@@ -1,12 +1,17 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Screen extends JPanel {
+public class Screen extends JPanel implements ActionListener {
 
 	//Board Info:
 	private final int screenHeight = 500;
@@ -23,6 +28,7 @@ public class Screen extends JPanel {
 	private int numFood = 1; //Number of food available. Note: Maybe add this later. Use for loop to create them! 
 	private final int foodX[] = new int[numFood];
 	private final int foodY[] = new int[numFood];
+	private Random rand;
 	
 	//Movement Info:
 	boolean left = false;
@@ -39,6 +45,7 @@ public class Screen extends JPanel {
 		initScreen();
 	}
 
+	//Initialize the game screen
 	private void initScreen() {
 		
         addKeyListener(new MyKeyListener1());
@@ -48,6 +55,97 @@ public class Screen extends JPanel {
         initGame();
 		
 	}
+	
+	//Initialize the actual game
+	private void initGame() {
+		
+		//Build snake's starting body
+		for (int i = 0; i < snakeSize; i++) {
+			snakeX[i] = i * 10 + 250; //Note: setting the 250 to 500 pushes snake off
+			snakeY[i] = 250; //Note: Setting this to 500 pushes snake off
+		}
+		
+		//Add food to screen
+		//When initializing game, we need to add food randomly based on numFood.
+		for (int i = 0; i < numFood; i++) {
+			changeFood(i);
+		}
+		
+		gameInProgress = true;
+		
+		//Begin the timer so that movement and game is set in motion!
+		gameTimer = new Timer(100, this);
+		gameTimer.start();
+		
+	}
+	
+	private boolean checkNewFoodPanel(int i) {
+		for (int j = 0; j < snakeSize; j++) {
+			if (foodX[i] == snakeX[j] && foodY[i] == snakeY[j]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private void changeFood(int j) {
+		foodX[j] = (int)(Math.random() * 50) * 10;
+		foodY[j] = (int)(Math.random() * 50) * 10;
+		while(!checkNewFoodPanel(j)) {
+			foodX[j] = (int)(Math.random() * 50) * 10;
+			foodY[j] = (int)(Math.random() * 50) * 10;
+		}
+	}
+	
+	
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+    
+    private void doDrawing(Graphics g) {
+        
+        if (gameInProgress) {
+
+        	//draw the snake
+        	
+        	//Set the colour for the snake. (Maybe let the user change this!)
+			g.setColor(Color.RED);
+        	for (int i = 0; i < snakeSize; i++) {
+    			g.drawRect(snakeX[i], snakeY[i], panelSize, panelSize);
+    			g.fillRect(snakeX[i], snakeY[i], panelSize, panelSize);
+    		}
+        	
+        	//draw the food
+        	
+        	//Set food colour (Maybe let user change this as well!)
+        	g.setColor(Color.GREEN);
+        	for (int j = 0; j < numFood; j++) {
+    			g.drawRect(foodX[j], foodY[j], panelSize, panelSize);
+    			g.fillRect(foodX[j], foodY[j], panelSize, panelSize);
+        	}
+        	
+            Toolkit.getDefaultToolkit().sync();
+
+        } else {
+        	//Game Over scenario!
+        	
+        }        
+    }
+	
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (gameInProgress) {
+
+            //checkFoodHit();
+            //checkSnakeHit();
+            //move();
+        }
+
+        repaint();
+    }
 	
 	private class MyKeyListener1 extends KeyAdapter {
 		
